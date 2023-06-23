39fde879a9c0bbc966e77511322825d38a1641fd
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-23 12:32:34 -0600

    Add list rentals method

--- app.rb
+++ app.rb
@@ -11,4 +11,18 @@ class App
     @rentals = []
     puts "Welcome to School Library App!\n\n"
   end
+
+  def list_rentals_for_person_id
+    print 'ID of person: '
+    id = gets.chomp.to_i
+
+    rentals = @rentals.filter { |rental| rental.person.id == id }
+    puts 'Rentals:'
+
+    rentals.each do |rental|
+      puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+      puts
+    end
+  end
+
 end
