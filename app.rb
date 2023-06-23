323ee3c647ef2c4ae37b6cb89d8090d38d81e814
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-23 12:23:08 -0600

    Add class App

--- app.rb
+++ app.rb
@@ -4,3 +4,11 @@ require_relative 'teacher'
 require_relative 'student'
 require_relative 'rental'
 
+class App
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+    puts "Welcome to School Library App!\n\n"
+  end
+end
