33bb6a7b1da0dd089215f85225143b5aaf5ec526
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-22 18:55:46 -0600

    Add classroom method into student.rb

--- student.rb
+++ student.rb
@@ -1,10 +1,19 @@
 require_relative 'person'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
+  end
+end
 
   def play_hooky
     '¯\\_(ツ)_/¯'
