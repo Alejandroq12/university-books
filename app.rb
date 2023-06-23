cc512e5428c213022038445f75301aed62399caf
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-23 12:33:41 -0600

    Add list all people method

--- app.rb
+++ app.rb
@@ -30,4 +30,10 @@ class App
     puts
   end
 
+  def list_all_people
+    @people.each do |person|
+      puts "Type: #{person.class}, Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+    end
+    puts
+  end
 end
