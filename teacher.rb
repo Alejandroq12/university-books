95b894ddf17b278874aa99a09a758e07b37a4cc9
Julio Quezada <julio.quezada@catolica.edu.sv>
2023-06-23 12:19:32 -0600

    Refactor teacher class in order to make the app work

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,10 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(age, name, specialization, parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
-  end
+  end 
 
   def can_use_services?
     true
